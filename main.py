14b02a1e2fbc2de6838a4def4bf95ae38ab3d9c3
GerardoBZentenoM <brallanzenteno@gmail.com>
2022-03-31 23:31:06 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+/* #############################################################################
+**    File           : main.py
+**    Proyect        : DoS-BruteForce-FakeData-Attack
+**    IDE            : Visual Studio Code
+**    Language       : Python 3.9.7
+**    Date/Hour      : 03/31/2022
+**    Description    :
+**         A Python tool to perform DoS, brute force and fake data fill attacks
+**         For educational purposes only*
+**   Authors         :
+**            Gerardo B Zenteno M. https://github.com/GerardoBZentenoM
+**
+**   Versión        : Beta
+**   Revisión       : A
+**   Release        : 0.0.1
+**   Bugs & Fixes   :
+**
+**   Date           :
+**
+**
+**   Usage          : sudo python3 main.py
+   ###########################################################################*/
+"""
+
+
+def test():
+    print("Hi")
